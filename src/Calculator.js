4c544df4eb6d583751bc1237f217825eb8c41b4e
Thomas Semiz <tas56@njit.edu>
2021-02-13 21:07:36 -0500

    Added calculator and tests

--- src/Calculator.js
+++ src/Calculator.js
@@ -6,24 +6,26 @@ const Quotient = require('./Operations/quotient');
 
 class Calculator {
 
+    static Calculations = [];
+
     static Sum(a,b){
         let calculation = new Calculation(a,b,Sum);
-
+        this.Calculations.push(Calculation);
         return calculation.GetResults();
     }
     static Difference(a,b){
         let calculation = new Calculation(a,b,Difference);
-
+        this.Calculations.push(Calculation);
         return calculation.GetResults();
     }
     static Product(a,b){
         let calculation = new Calculation(a,b,Product);
-
+        this.Calculations.push(Calculation);
         return calculation.GetResults();
     }
     static Quotient(a,b){
         let calculation = new Calculation(a,b,Quotient);
-
+        this.Calculations.push(Calculation);
         return calculation.GetResults();
     }
 
--- tests/Calculator.test.js
+++ tests/Calculator.test.js
@@ -1,15 +1,28 @@
-const Calculation = require('../src/models/Calculation');
-const Sum = require('../src/Operations/sum');
-const Difference = require('../src/Operations/difference');
-const Product = require('../src/Operations/product');
-const Quotient = require('../src/Operations/quotient');
+const Calculator = require('../src/Calculator');
 
-test('Test of Calculation instantiation', () => {
-    let op = Sum;
-
-    let calculation = new Calculation(1,2,op);
-    expect(calculation.a).toBe(1);
-    expect(calculation.b).toBe(2);
-    expect(calculation.op).toBe(op);
+test('Calculator adding two numbers', () => {
+    let result = Calculator.Sum(1,2);
+    expect(result).toBe(3);
+});
+test('Calculator difference of two numbers', () => {
+    let result = Calculator.Difference(1,2);
+    expect(result).toBe(-1);
+});
+test('Calculator multiplying two numbers', () => {
+    let result = Calculator.Product(1,2);
+    expect(result).toBe(2);
+});
+test('Calculator dividing two numbers', () => {
+    let result = Calculator.Quotient(1,2);
+    expect(result).toBe(.5);
+});
+test('Calculator adding to calculations', () => {
+    let result = Calculator.Quotient(1,2);
+    // let calculations = Calculator.Calculations;
+    //
+    // calculations.forEach( (calculation => {
+    //     console.log(calculation.GetResults());
+    // })
+    expect(result).toBe(.5);
 });
 
